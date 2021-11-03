227c055b292eff328000329411d61d6fb835d3f7
halezmarie <teacherhaleymarie@gmail.com>
2021-11-02 21:59:17 -0500

    commit tree 75368091e4bb1098b5fe1f45dba3e49d3ddbc0bc

--- src/components/Restaurants.js
+++ src/components/Restaurants.js
@@ -1,24 +1,65 @@
 import React, { Component } from 'react';
+import { addRestaurant } from '../actions/restaurants';
 import { connect } from 'react-redux';
 
-class Restaurants extends Component {
+export class RestaurantInput extends Component {
+
+  state = {
+    name: '',
+    location: ''
+  }
+
+  handleOnNameChange = event => {
+    this.setState({
+      name: event.target.value
+    });
+  }
+
+  handleOnLocationChange = event => {
+    this.setState({
+      location: event.target.value
+    });
+  }
+
+  handleOnSubmit = event => {
+    event.preventDefault();
+    this.props.addRestaurant(this.state);
+  }
 
   render() {
-
-    let restaurants = this.props.restaurants.map((restaurant, index) => <li key={index}>{restaurant.name}</li>);
-
-    return (
-      <div>
-        <ul>
-          {restaurants}
-        </ul>
-      </div>
+    return(
+      <form onSubmit={(event) => this.handleOnSubmit(event)}>
+        <p>
+          <input
+            type="text"
+            onChange={(event) => this.handleOnNameChange(event)}
+            value={this.state.name}
+            placeholder="restaurant name" />
+        </p>
+        <p>
+          <input
+            type="text"
+            onChange={(event) => this.handleOnLocationChange(event)}
+            value={this.state.location}
+            placeholder="location" />
+        </p>
+        <input type="submit" />
+      </form>
     );
   }
 };
 
-const mapStateToProps = state => {
-  return { restaurants: state.restaurants }
-}
+const mapDispatchToProps = dispatch => {
+  return {
+    addRestaurant: (newRestaurant) => {
+      dispatch(addRestaurant(newRestaurant))
+    }
+  };
+};
 
-export default connect(mapStateToProps)(Restaurants);
+
+export default connect(null, mapDispatchToProps)(RestaurantInput);
+
+// Alternative form:
+
+// export default connect(null, { addRestaurant })(RestaurantInput);
